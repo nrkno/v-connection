17f1123115984d746481d5f9025cfb65693cd710
Balte de Wit <contact@balte.nl>
2021-10-19 13:14:11 +0200

    fix: don't throw away middle chunks

--- src/peptalk.ts
+++ src/peptalk.ts
@@ -395,7 +395,7 @@ class PepTalk extends EventEmitter implements PepTalkClient, PepTalkJS {
 		}
 		// console.log('SAF >>>', split)
 		if (this.leftovers) {
-			this.leftovers.previous = this.leftovers.previous + split[0]
+			this.leftovers.previous = this.leftovers.previous + (split[0] || leftovers?.previous)
 			if (Array.isArray(split) && split.length > 0) {
 				this.leftovers.remaining -= Buffer.byteLength(split[0], 'utf8')
 			}
